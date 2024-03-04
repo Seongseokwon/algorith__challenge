c676cdcddaa63d2b173d5abf84cc171f386d181d
SeongSeokwon <pam9411@naver.com>
2024-03-04 21:27:35 +0900

    멀리

--- 2024_03/03_04.test.js
+++ 2024_03/03_04.test.js
@@ -1,15 +1,26 @@
+const { incorrect_멀리뛰기 } = require("./멀리뛰기");
 const { incorrect_내맘대로정렬, correct_내맘대로정렬 } = require("./문자열_내맘대로_정렬하기");
 
-describe('3월 4일 문제 테스트', () => {
-  it('내맘대로정렬1', () => {
+describe('내맘대로_정렬하기', () => {
+  it('Test Case 1', () => {
     expect(correct_내맘대로정렬(["sun", "bed", "car"], 1)).toEqual(["car", "bed", "sun"]);
     expect(correct_내맘대로정렬(["abce", "abcd", "cdx"], 2)).toEqual(["abcd", "abce", "cdx"]);
     
   });
   
-  it('내맘대로정렬2', () => {
+  it('Test Case 2', () => {
     expect(incorrect_내맘대로정렬(["sun", "bed", "car"], 1)).toEqual(["car", "bed", "sun"]);
     expect(incorrect_내맘대로정렬(["abce", "abcd", "cdx"], 2)).toEqual(["abcd", "abce", "cdx"]);
     
   });
+})
+
+describe('멀리 뛰기', () => {
+  it('Test Case 1', () => {
+    expect(incorrect_멀리뛰기(4)).toBe(5);
+  });
+
+  it('Test Case 2', () => {
+    expect(incorrect_멀리뛰기(3)).toBe(3);
+  })
 })
---
+++ 2024_03/멀리뛰기.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @param {number} n 총 칸수
+ * 
+ * 1칸, 2칸 뛸 수 있다.
+ * 총 칸수를 입력받았을 때, 몇가지 방법으로 끝에 도달할수 있는지 구하기.
+ */
+const 멀리뛰기 = (n) => {
+  const diff = new Array(n).fill(0);
+
+  diff[0] = 1n;
+  diff[1] = 1n;
+  diff[2] = 2n;  
+  
+  for(let i = 3; i <= n ; i++) {
+    diff[i] = diff[i - 1 ] + diff[i - 2];
+  }
+  
+  
+  return Number(diff[n] % 1234567n);
+}
+
+incorrect_멀리뛰기(3);
+module.exports = {멀리뛰기};
